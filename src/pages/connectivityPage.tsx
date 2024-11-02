3badf304453d78ce7cde6af153be2b8294c409f1
ermal <ermal@91.life>
2024-11-02 20:47:54 +0100

    connectivity

--- src/pages/connectivityPage.tsx
+++ src/pages/connectivityPage.tsx
@@ -4,6 +4,7 @@ const ConnectivityPage = () => {
     <div>
       <h1 className="text-2xl font-bold mb-4">Connectivity Summary</h1>
       <p>Connectivity status and information</p>
+      <p>10% are connected</p>
     </div>
   );
 };
